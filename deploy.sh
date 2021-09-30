2f9fdae3629bda7d8d5a9d671cf9ba994c4cec43
Theodore Keloglou <zf@sirodoht.com>
2021-09-30 15:42:47 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -11,10 +11,8 @@ hugo # generate anew
 scp ./status.mataroa.blog.conf root@evey.sirodoht.com:/etc/nginx/sites-available/
 
 # remove old and copy over new html files
-ssh root@evey.sirodoht.com rm -r /var/www/status.mataroa.blog/
-ssh root@evey.sirodoht.com mkdir -p /var/www/status.mataroa.blog/html
+ssh root@evey.sirodoht.com 'rm -r /var/www/status.mataroa.blog/ && mkdir -p /var/www/status.mataroa.blog/html'
 rsync -rsP ./public/ root@evey.sirodoht.com:/var/www/status.mataroa.blog/html
 
 # restart nginx
-ssh root@evey.sirodoht.com 'nginx -t'
-ssh root@evey.sirodoht.com 'systemctl reload nginx'
+ssh root@evey.sirodoht.com 'nginx -t && systemctl reload nginx'
